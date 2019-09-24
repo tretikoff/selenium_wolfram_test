dc020c2f1f82ffbe4b7aebfcedb486cbc3f1c43f
tretikoff <treticoff@gmail.com>
2019-09-24 20:45:06 +0300

    '3'

---
+++ src/test/java/lab/util/CSVWriterTest.java
@@ -0,0 +1,24 @@
+package lab.util;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class CSWriterTest {
+
+    private static final BigDecimal TWO = BigDecimal.valueOf(2L);
+
+    public static BigDecimal sqrt(BigDecimal x, MathContext mc) {
+        BigDecimal g = x.divide(TWO, mc);
+        boolean done = false;
+        final int maxIterations = mc.getPrecision() + 1;
+        for (int i = 0; !done && i < maxIterations; i++) {
+            // r = (x/g + g) / 2
+            BigDecimal r = x.divide(g, mc);
+            r = r.add(g);
+            r = r.divide(TWO, mc);
+            done = r.equals(g);
+            g = r;
+        }
+        return g;
+    }
+}
